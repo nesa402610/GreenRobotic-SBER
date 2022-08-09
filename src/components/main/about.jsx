723acc9d019b3eb870ddaa5a8a333d3c5b67d23a
nesa402610 <argyleferris@gmail.com>
2022-08-09 15:01:15 +0300

    Финальный результат. Добавлены анимации и либы

--- src/components/main/about.jsx
+++ src/components/main/about.jsx
@@ -5,14 +5,14 @@ import solarpanel from "../../img/solar-panel.png";
 const About = () => {
     return (
         <section className="about">
-            <div className="about__info">
+            <div className="about__info animate__animated animate__fadeInLeft animate__slow">
                 <h1>Солнечные панели GreenRobotic</h1>
                 <p>Производите дешёвую экологически чистую электроэнергию из солнечного света</p>
-                <button>Узнать подробнее</button>
+                <button>Узнать подробнее <i className="fas fa-angle-right"/></button>
             </div>
-            <div className="about__images">
+            <div className="about__images animate__animated animate__fadeInRight animate__slow">
                 <img className="about__sun-image" src={sun}/>
-                <img className="about__image" src={solarpanel}/>
+                    <img className="about__image" src={solarpanel}/>
             </div>
         </section>
     );
